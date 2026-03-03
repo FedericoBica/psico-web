11fd02670e3da378196223a7ca7c22739b4ee924
fedebi1 <fedebicasua@gmail.com>
2026-03-03 15:24:35 -0300

    fix

--- nextauth.d.ts
+++ nextauth.d.ts
@@ -1,17 +1,14 @@
 import NextAuth, { DefaultSession } from 'next-auth';
 
-
-
-
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name: string;
-      email: string;
-      emailVerified?: boolean;
       role: string;
-      image?: string;
-    } & DefaultSession['user'];
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    role: string;
   }
 }
