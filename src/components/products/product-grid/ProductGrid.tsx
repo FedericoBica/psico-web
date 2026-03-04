5a512c24c821ac9b7ffb05c04632a961ec51daae
fedebi1 <fedebicasua@gmail.com>
2026-03-04 11:11:16 -0300

    fix2

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -5,12 +5,11 @@ interface Props {
   products: Product[];
 }
 
-
-export const ProductGrid = ( { products = [] }: Props ) => {
-  if (!products || products.length === 0) return <p>No hay productos disponibles.</p>;
+export const ProductGrid = ({ products = [] }: Props) => {
+  if (!products || products.length === 0) return <p className="text-center py-10">No hay productos disponibles.</p>;
+  
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-10 mb-10 px-4 sm:px-0">
-      {
+    <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 sm:gap-10 mb-10 px-0">      {
         products.map( product => (
           <ProductGridItem
             key={ product.slug }
@@ -18,7 +17,6 @@ export const ProductGrid = ( { products = [] }: Props ) => {
           />
         ) )
       }
-
     </div>
   );
 };
