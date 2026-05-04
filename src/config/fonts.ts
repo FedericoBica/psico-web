042f6cfe92728b20f67d4c39489de844ba095405
fedebi1 <fedebicasua@gmail.com>
2026-05-04 14:19:27 -0300

    fix

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,14 +1,21 @@
-import { Inter, Montserrat_Alternates } from 'next/font/google';
-
-
-
+import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const inter = Inter({ subsets: ['latin'] });
 
-
-export const titleFont = Montserrat_Alternates({ 
-  subsets: ['latin'],
-  weight: ['500', '700'],
+export const titleFont = localFont({
+  src: [
+    {
+      path: '../../public/fonts/montserrat-alternates-500.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/montserrat-alternates-700.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
   display: 'swap',
-  fallback: ['system-ui','arial']
+  fallback: ['system-ui', 'arial'],
 });
